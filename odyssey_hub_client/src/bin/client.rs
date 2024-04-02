1b24f3ca2fbed413ba1df1b62577380b58c9daeb
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-04-02 17:19:33 -0500

    Move ctrl c logic outside of lib for client

--- odyssey_hub_client/src/bin/client.rs
+++ odyssey_hub_client/src/bin/client.rs
@@ -1,6 +1,26 @@
+use std::sync::Arc;
+
 use odyssey_hub_client::client;
 
 #[tokio::main]
 pub async fn main() -> anyhow::Result<()> {
-    client::run().await
+    let client = Arc::new(client::Client::default());
+    let res = tokio::try_join!(
+        tokio::spawn({ let client = client.clone(); async move { client.run().await } }),
+        tokio::spawn({
+            let end_token = client.end_token.clone();
+            async move {
+                tokio::signal::ctrl_c().await.unwrap();
+                end_token.cancel();
+            }
+        })
+    );
+    match res {
+        Ok((_, _)) => {
+            Ok(())
+        }
+        Err(err) => {
+           anyhow::bail!("processing failed; error = {}", err);
+        }
+    }
 }
--- odyssey_hub_client/src/client.rs
+++ odyssey_hub_client/src/client.rs
@@ -2,58 +2,58 @@ use interprocess::local_socket::{tokio::prelude::LocalSocketStream, traits::toki
 use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, try_join};
 use tokio_util::sync::CancellationToken;
 
-pub async fn run() -> anyhow::Result<()> {
-    let name = {
-        use NameTypeSupport::*;
-        match NameTypeSupport::query() {
-            OnlyFs => "/tmp/odyhub.sock".to_fs_name(),
-            OnlyNs | Both => "@odyhub.sock".to_ns_name(),
-        }
-    };
-    let name = name?;
+#[derive(Default)]
+pub struct Client {
+    pub end_token: CancellationToken,
+}
 
-    // Await this here since we can't do a whole lot without a connection.
-    let conn = LocalSocketStream::connect(name).await?;
-
-    // This consumes our connection and splits it into two halves,
-    // so that we could concurrently act on both.
-    let (reader, mut writer) = conn.split();
-    let mut reader = BufReader::new(reader);
-
-    // Allocate a sizeable buffer for reading.
-    // This size should be enough and should be easy to find for the allocator.
-    let mut buffer = String::with_capacity(128);
-
-    let end_token = CancellationToken::new();
-
-    let task = tokio::spawn({
-        let end_token = end_token.clone();
-        async move {
-            while !end_token.is_cancelled() {
-                // Describe the write operation as writing our whole string.
-                let write = writer.write_all(b"Hello from client!\n");
-                // Describe the read operation as reading until a newline into our buffer.
-                let read = reader.read_line(&mut buffer);
-
-                // Concurrently perform both operations.
-                try_join!(write, read).unwrap();
-
-                // Display the results when we're done!
-                println!("Server answered: {}", buffer.trim());
-
-                tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+impl Client {
+    pub async fn run(&self) -> anyhow::Result<()> {
+        let name = {
+            use NameTypeSupport::*;
+            match NameTypeSupport::query() {
+                OnlyFs => "/tmp/odyhub.sock".to_fs_name(),
+                OnlyNs | Both => "@odyhub.sock".to_ns_name(),
             }
-            writer.write_all(b"exit\n").await.unwrap();
-            drop((reader, writer));
-        }
-    });
+        };
+        let name = name?;
 
-    tokio::spawn(async move {
-        tokio::signal::ctrl_c().await.unwrap();
-        end_token.cancel();
-    }).await?;
+        // Await this here since we can't do a whole lot without a connection.
+        let conn = LocalSocketStream::connect(name).await?;
 
-    task.await?;
+        // This consumes our connection and splits it into two halves,
+        // so that we could concurrently act on both.
+        let (reader, mut writer) = conn.split();
+        let mut reader = BufReader::new(reader);
 
-    Ok(())
+        // Allocate a sizeable buffer for reading.
+        // This size should be enough and should be easy to find for the allocator.
+        let mut buffer = String::with_capacity(128);
+
+        let task = tokio::spawn({
+            let end_token = self.end_token.clone();
+            async move {
+                while !end_token.is_cancelled() {
+                    // Describe the write operation as writing our whole string.
+                    let write = writer.write_all(b"Hello from client!\n");
+                    // Describe the read operation as reading until a newline into our buffer.
+                    let read = reader.read_line(&mut buffer);
+
+                    // Concurrently perform both operations.
+                    try_join!(write, read).unwrap();
+
+                    // Display the results when we're done!
+                    println!("Server answered: {}", buffer.trim());
+
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+                writer.write_all(b"exit\n").await.unwrap();
+                drop((reader, writer));
+            }
+        });
+
+        task.await?;
+
+        Ok(())
+    }
 }
