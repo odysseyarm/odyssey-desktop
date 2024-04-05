677869a2fed0f8ba00883b3d3d144b158ac77a81
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-04-05 13:42:34 -0500

    Make service go into running and stopped status

--- odyssey_hub_service/src/bin/service/service.rs
+++ odyssey_hub_service/src/bin/service/service.rs
@@ -1,11 +1,11 @@
-use std::ffi::OsString;
+use std::{ffi::OsString, time::Duration};
 
 use interprocess::local_socket::{tokio::prelude::{LocalSocketListener, LocalSocketStream}, traits::tokio::{Listener, Stream}, NameTypeSupport, ToFsName, ToNsName};
-use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, try_join};
+use tokio::{io::{AsyncBufReadExt, AsyncWriteExt, BufReader}, sync::mpsc, try_join};
 
 #[cfg(target_os = "windows")]
 use windows_service::define_windows_service;
-use windows_service::{service_dispatcher};
+use windows_service::{service::{ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus, ServiceType}, service_control_handler::{self, ServiceControlHandlerResult, ServiceStatusHandle}, service_dispatcher};
 
 #[cfg(target_os = "windows")]
 define_windows_service!(ffi_service_main, service_main);
@@ -23,8 +23,99 @@ pub fn start() {
     }
 }
 
+enum Message {
+    ServerInit(Result<(), std::io::Error>),
+    Stop,
+}
+
 #[tokio::main]
-async fn service_main(arguments: Vec<OsString>) -> anyhow::Result<()> {
+async fn service_main(_arguments: Vec<OsString>) -> Result<(), windows_service::Error> {
+    let (sender, receiver) = mpsc::unbounded_channel();
+
+    let event_handler = {
+        let sender = sender.clone();
+        move |control_event| -> ServiceControlHandlerResult {
+            match control_event {
+                ServiceControl::Stop => {
+                    // Handle stop event and return control back to the system.
+                    sender.send(Message::Stop).unwrap();
+                    ServiceControlHandlerResult::NoError
+                }
+                // All services must accept Interrogate even if it's a no-op.
+                ServiceControl::Interrogate => ServiceControlHandlerResult::NoError,
+                _ => ServiceControlHandlerResult::NotImplemented,
+            }
+        }
+    };
+
+    // Register system service event handler
+    let status_handle = service_control_handler::register("OdysseyService", event_handler)?;
+
+    let running_status = ServiceStatus {
+        // Should match the one from system service registry
+        service_type: ServiceType::OWN_PROCESS,
+        // The new state
+        current_state: ServiceState::Running,
+        // Accept stop events when running
+        controls_accepted: ServiceControlAccept::STOP,
+        // Used to report an error when starting or stopping only, otherwise must be zero
+        exit_code: ServiceExitCode::Win32(0),
+        // Only used for pending states, otherwise must be zero
+        checkpoint: 0,
+        // Only used for pending states, otherwise must be zero
+        wait_hint: Duration::default(),
+        // Unused for setting status
+        process_id: None,
+    };
+
+    tokio::select! {
+        _ = tokio::spawn(run_service(sender)) => {},
+        _ = tokio::spawn({
+                let running_status = running_status.clone();
+                async move {
+                    handle_service_status(status_handle, receiver, running_status).await;
+                }
+            }) => {}
+    };
+
+    // Tell the system that service has stopped.
+    status_handle.set_service_status(ServiceStatus {
+        service_type: ServiceType::OWN_PROCESS,
+        current_state: ServiceState::Stopped,
+        controls_accepted: ServiceControlAccept::empty(),
+        exit_code: ServiceExitCode::Win32(0),
+        checkpoint: 0,
+        wait_hint: Duration::default(),
+        process_id: None,
+    })?;
+
+    Ok(())
+}
+
+async fn handle_service_status(
+    status_handle: ServiceStatusHandle,
+    mut receiver: mpsc::UnboundedReceiver<Message>,
+    running_status: ServiceStatus,
+) {
+    loop {
+        match receiver.recv().await {
+            Some(Message::ServerInit(Ok(()))) => {
+                status_handle.set_service_status(running_status.clone()).unwrap();
+            }
+            Some(Message::ServerInit(Err(_))) => {
+                break;
+            }
+            Some(Message::Stop) => {
+                break;
+            }
+            None => {
+                break;
+            }
+        }
+    }
+}
+
+async fn run_service(sender: mpsc::UnboundedSender<Message>) -> anyhow::Result<()> {
     // Describe the things we do when we've got a connection ready.
     async fn handle_conn(conn: LocalSocketStream) -> std::io::Result<()> {
         // Split the connection into two halves to process
@@ -70,6 +161,8 @@ async fn service_main(arguments: Vec<OsString>) -> anyhow::Result<()> {
     // ensure it's a socket file and not a normal file, and delete it.
     let listener = LocalSocketListener::bind(name.clone())?;
     println!("Server running at {:?}", name.clone());
+
+    sender.send(Message::ServerInit(Ok(()))).unwrap();
 
     // Set up our loop boilerplate that processes our incoming connections.
     loop {
