026a8d71df589cfa764e01553fa0f32ddd3222cd
Abraham Hamidi <abraham.hamidi01@gmail.com>
2024-04-06 18:10:07 -0500

    Build.rs a little less shit

--- odyssey_hub_service/build.rs
+++ odyssey_hub_service/build.rs
@@ -1,9 +1,5 @@
-#[cfg(windows)]
-use winres;
-
 #[cfg(target_os = "windows")]
 fn main() {
-    use std::io::Write;
     // only build the resource for release builds
     // as calling rc.exe might be slow
     if std::env::var("PROFILE").unwrap() == "release" {
@@ -22,12 +18,9 @@ fn main() {
 </assembly>
 "#,
             );
-        match res.compile() {
-            Err(error) => {
-                write!(std::io::stderr(), "{}", error).unwrap();
-                std::process::exit(1);
-            }
-            Ok(_) => {}
+        if let Err(error) = res.compile() {
+            eprintln!("{}", error);
+            std::process::exit(1);
         }
     }
 }
