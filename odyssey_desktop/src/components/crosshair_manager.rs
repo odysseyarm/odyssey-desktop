e35896ca199651c8313709a8c8cc5494f3a4b445
Abraham Hamidi <abraham.hamidi01@gmail.com>
2025-05-28 19:31:02 -0500

    Transform the normalized coordinates correctly

--- odyssey_desktop/src/components/crosshair_manager.rs
+++ odyssey_desktop/src/components/crosshair_manager.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 
-use dioxus::prelude::*;
-
+use dioxus::{html::geometry::euclid::Rect, prelude::*};
 use odyssey_hub_common::events as oe;
 
 pub type PlayerId = [u8; 6];
@@ -23,67 +22,74 @@ const CROSSHAIRS: [Asset; 6] = [
 
 #[component]
 pub fn CrosshairManager(hub: Signal<crate::hub::HubContext>) -> Element {
-    // debug
-    // let mut players = use_signal(|| vec![Player { id: [0; 6], pos: (0.0, 0.0) }]);
+    // 1) players + seen
     let mut players = use_signal(Vec::new);
-
     let mut seen = use_signal(HashSet::new);
 
+    // 2) signal to hold the container's Rect
+    let container_rect = use_signal(Rect::zero);
+
+    // 3) onmounted on root <div> to measure once
+    let mut rect_signal = container_rect.clone();
+
+    // 4) event loop: read new events, map normalized aimpoint → pixel coords
     use_effect(move || {
+        let rect = container_rect.read();
+        let width = rect.width() as f64;
+        let height = rect.height() as f64;
+        dioxus::logger::tracing::info!("rect: {:?}", rect);
+        let origin_x = rect.origin.x as f64;
+        let origin_y = rect.origin.y as f64;
+
         for event in hub().events.read().iter() {
             if let oe::Event::DeviceEvent(oe::DeviceEvent {
                 device,
                 kind: oe::DeviceEventKind::TrackingEvent(oe::TrackingEvent { aimpoint, .. }),
             }) = event
             {
+                let x = origin_x + (aimpoint.x as f64) * width;
+                let y = origin_y + (aimpoint.y as f64) * height;
+
                 let id = device.uuid();
                 if seen.write().insert(id) {
-                    players.push(Player {
-                        id,
-                        pos: (aimpoint.x as f64, aimpoint.y as f64),
-                    });
-                } else {
-                    if let Some(mut player) = players.iter_mut().find(|p| p.id == id) {
-                        // todo figure out how to transform aimpoint
-                        player.pos = (aimpoint.x as f64 * 200., aimpoint.y as f64 * 200.);
-                    }
+                    players.push(Player { id, pos: (x, y) });
+                } else if let Some(mut p) = players.iter_mut().find(|p| p.id == id) {
+                    p.pos = (x, y);
                 }
             }
         }
     });
 
-    let children = players
-        .iter()
-        .enumerate()
-        .map(|(i, player)| {
-            let key = player.id.iter()
-                .map(|b| format!("{:02x}", b))
-                .collect::<String>();
-            rsx! {
-                img {
-                    key: "{key}",
-                    class: "absolute pointer-events-none translate-x-[-50%] translate-y-[-50%]",
-                    transform_origin: "center",
-                    transform: format!("translate({:.0}px, {:.0}px)", player.pos.0, player.pos.1),
-                    src: CROSSHAIRS[i.min(CROSSHAIRS.len() - 1)],
-                    alt: "crosshair"
-                }
+    let children = players.iter().enumerate().map(|(i, player)| {
+        let key = player
+            .id
+            .iter()
+            .map(|b| format!("{:02x}", b))
+            .collect::<String>();
+        rsx! {
+            img {
+                key: "{key}",
+                class: "absolute pointer-events-none translate-x-[-50%] translate-y-[-50%]",
+                transform_origin: "center",
+                transform: format!("translate({:.0}px, {:.0}px)", player.pos.0, player.pos.1),
+                src: CROSSHAIRS[i.min(CROSSHAIRS.len() - 1)],
+                alt: "crosshair"
             }
-        });
+        }
+    });
 
     rsx! {
         div {
-            class: "absolute inset-0",
-            // debug
-            // onmousemove: move |evt: MouseEvent| {
-            //     let coords = evt.data;
-            //     let mut list = players.write();
-            //     if let Some(p) = list.get_mut(0) {
-            //         let ec = coords.element_coordinates();
-            //         p.pos = (ec.x, ec.y);
-            //     }
-            // },
-            // end debug
+            class: "absolute inset-0 overflow-hidden",
+            onmounted: move |cx| async move {
+                // todo cheap trick
+                tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+                let cx_data = cx.data();
+                let client_rect = cx_data.as_ref().get_client_rect();
+                if let Ok(rect) = client_rect.await {
+                    rect_signal.set(rect);
+                }
+            },
             {children}
         }
     }
